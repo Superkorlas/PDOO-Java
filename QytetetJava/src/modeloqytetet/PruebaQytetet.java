7a56cfc3c62a5f0ffabbdfbfedb56e916f77551a
Superkorlas <alejandro@ei140175>
2018-09-17 20:18:39 +0200

    Acabada parte s1 Java

--- QytetetJava/src/modeloqytetet/PruebaQytetet.java
+++ QytetetJava/src/modeloqytetet/PruebaQytetet.java
@@ -5,17 +5,70 @@
  */
 package modeloqytetet;
 
+import java.util.ArrayList;
+
+
 /**
  *
  * @author alejandro
  */
 public class PruebaQytetet {
+    static Qytetet juego = new Qytetet();
 
+    static private ArrayList<Sorpresa> Mayor0(){
+       ArrayList<Sorpresa> resultado = new ArrayList<>();
+       
+       for (Sorpresa it : juego.getMazo()) {
+           if(it.getValor() > 0){
+               resultado.add(it);
+           }
+       }
+       return resultado;
+    }
+    
+    static private ArrayList<Sorpresa> TipoIrCasilla(){
+       ArrayList<Sorpresa> resultado = new ArrayList<>();
+       
+       for (Sorpresa it : juego.getMazo()) {
+           if(it.getTipo() == TipoSorpresa.IRACASILLA) {
+               resultado.add(it);
+           }
+       }
+       return resultado;
+    }
+    
+    static private ArrayList<Sorpresa> DevuelveTipo(TipoSorpresa tipo){
+       ArrayList<Sorpresa> resultado = new ArrayList<>();
+       
+       for (Sorpresa it : juego.getMazo()) {
+           if(it.getTipo() == tipo) {
+               resultado.add(it);
+           }
+       }
+       return resultado;
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+       juego.InicializarCartas();
+       
+       for (Sorpresa it : juego.getMazo()) {
+           System.out.println(it.toString());
+       }
+       
+       for (Sorpresa it : Mayor0()) {
+           System.out.println(it.toString());
+       }
+              
+       for (Sorpresa it : TipoIrCasilla()) {
+           System.out.println(it.toString());
+       }
+                     
+       for (Sorpresa it : DevuelveTipo(TipoSorpresa.PAGARCOBRAR)) {
+           System.out.println(it.toString());
+       }
     }
     
 }
