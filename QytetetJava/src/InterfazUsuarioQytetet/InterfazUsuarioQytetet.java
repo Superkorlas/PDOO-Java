dd130769cd4f10e2d2103b6e5772010a9e61561e
Alejandro de la Plata Ramos <superkorlas@gmail.com>
2018-11-05 19:10:03 +0100

    Sesión 4: diagrama de clases definido.

--- QytetetJava/src/InterfazUsuarioQytetet/InterfazUsuarioQytetet.java
+++ QytetetJava/src/InterfazUsuarioQytetet/InterfazUsuarioQytetet.java
@@ -7,6 +7,7 @@ package InterfazUsuarioQytetet;
 
 import modeloqytetet.Qytetet;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
@@ -16,7 +17,21 @@ public class InterfazUsuarioQytetet {
     Qytetet modelo = Qytetet.getInstance();
     
     public ArrayList<String> obtenerNombreJugadores() {
-        throw new UnsupportedOperationException("Sin implementar");
+        ArrayList<String> nombres = new ArrayList<>();
+        System.out.print("Introduce el numero de jugadores[2 a 4]:\n==>");
+        ArrayList<String> valoresCorrectos = new ArrayList();
+        valoresCorrectos.add("2");
+        valoresCorrectos.add("3");
+        valoresCorrectos.add("4");
+        int numJugadores = Integer.parseInt(this.leerValorCorrecto(valoresCorrectos));
+        System.out.println("Muy bien, habra " + numJugadores + " jugadores, ahora sus nombres:");
+        valoresCorrectos.clear();
+        for(int i=1; i<(numJugadores+1); i++){
+            System.out.print("Jugador" + i + ": ");
+            String s = leerValorCorrecto(valoresCorrectos);
+            nombres.add(s);
+        }
+        return nombres;
     }
     
     public int obtenerOpcionMenu(ArrayList<String> operacionesJuegoValidas) {
@@ -40,8 +55,14 @@ public class InterfazUsuarioQytetet {
     }
             
     public String leerValorCorrecto(ArrayList<String> valoresCorrectos) {
-        throw new UnsupportedOperationException("Sin implementar");
+        Scanner in = new Scanner(System.in);
+        String valor = new String();
+        do{
+            valor=in.next();
+        }while(!(valoresCorrectos.contains(valor)) || (valoresCorrectos.isEmpty() && (valor != "")));
+        return valor;
     }
+    
     public int elegirOperacion() {
         throw new UnsupportedOperationException("Sin implementar");
     }
